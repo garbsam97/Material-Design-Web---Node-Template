a3f03702d5a72b4d0731b0a49da4c1d7aae966f5
samuele garbuglia <samuele.garbuglia97@gmail.com>
2020-09-08 22:19:32 +0200

    First Commit

---
+++ webpack.client.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const buildDirectory = 'dist';
+const outputDirectory = buildDirectory + '/client';
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/scss/app.scss', './src/js/app.js'],
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css'
+                        },
+                    },
+                    { loader: 'extract-loader'},
+                    { loader: 'css-loader'},
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            webpackImporter: false,
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            }
+                        },
+                    },
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
